211b163697add2612c6a309ac96de9c6dccb87e8
William Wofford <27900656+scrunglium@users.noreply.github.com>
2024-06-29 00:29:17 -0500

    Added basic terrain brush and terrain body

--- scripts/terrain.gd
+++ scripts/terrain.gd
@@ -1,8 +1,12 @@
-class_name Terrain extends MeshInstance3D
+class_name Terrain extends Node3D
+
+@export var shape : CollisionShape3D
+@export var mesh : MeshInstance3D
 
 @export var known_size : float = 1.0
 @export var pixels_per_unit : float = 1.0
 
+var hmap_shape : HeightMapShape3D
 var image : Image
 var texture : ImageTexture
 
@@ -12,30 +16,27 @@ static var inst : Terrain
 func _ready() -> void:
 	inst = self
 	
-	var mat = mesh.surface_get_material(0) as ShaderMaterial
+	hmap_shape = shape.shape
+	
+	var mat = mesh.mesh.surface_get_material(0) as ShaderMaterial
 	texture = mat.get_shader_parameter("height") as ImageTexture
 	print(texture)
 	
 	var data = PackedByteArray()
-	data.resize(1024 * 1024 * 12)
+	data.resize(256 * 256 * 12)
 	image = Image.new()
-	image.set_data(1024, 1024, false, Image.FORMAT_RGBF, data)
+	image.set_data(256, 256, false, Image.FORMAT_RGBF, data)
 	texture.set_image(image)
 	pass # Replace with function body.
 
-func add_height(unit_position : Vector2, height : float, radius : float, falloff : float) -> void :
-	#print("unit_position: ", unit_position, ", unit_radius: ", radius)
-	
-	unit_position -= Vector2.ONE * radius * 0.5
+func add_height(unit_position : Vector2, height : float, radius : float, falloff : float) -> void :	
+	unit_position -= Vector2.ONE * radius
 	radius *= pixels_per_unit
 	unit_position *= pixels_per_unit
 	var unit_center = unit_position
 	
 	var rect_position = Vector2i(unit_position)
-	var rect_center = Vector2i(unit_center)
 	var rect_size : int = ceil(radius * 2)
-	
-	#print("pixel_position: ", rect_position, ", pixel_radius: ", radius)
 	
 	for ix in rect_size :
 		var x = rect_position.x + ix
@@ -51,17 +52,22 @@ func add_height(unit_position : Vector2, height : float, radius : float, falloff
 				continue
 			var ipos = Vector2i(ix, iy)
 			var pos = Vector2i(x, y)
-			var dist = (ipos - rect_center).length()
+			var dist = (ipos - Vector2i(rect_size, rect_size) / 2).length()
 			
 			if dist > radius : continue
+			
+			#var dist_alpha = clamp(remap(dist, 1 - falloff, dist / radius, 1.0, 0.0), 0.0, 1.0)
+			#var ih = height * dist_alpha
+			var ih = height * (1.0 - dist / radius)
 				
 			var c = image.get_pixelv(pos)
 			
-			c += Color(height, height, height)
+			c += Color(ih, ih, ih)
 			
 			image.set_pixelv(pos, c)
-	
+
 	texture.set_image(image)
+	hmap_shape.map_data = image.get_data().to_float32_array()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
